05338789299b1095a8a4c7710d9cedca54b68472
ezequielcuomo <ezequielcuomo@gmail.com>
2021-04-02 23:18:53 -0300

    primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { ContradorComponent } from './contador/contador.component';
+
+
+
+@NgModule({
+declarations:[
+ContradorComponent
+],
+exports:[
+ContradorComponent
+],
+imports:[
+
+]
+
+
+})
+
+export class ContadorModule { }
